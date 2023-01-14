e8a66688f547dbe619b65af91faa869d561cb779
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-01-15 01:42:42 +0530

    Added: Error Handling

--- src/WebHttp.mjs
+++ src/WebHttp.mjs
@@ -5,6 +5,7 @@ import { v4 } from 'uuid'
 import CONTEXT from './CONSTANTS/CONTEXT.mjs'
 import CryptoInterceptor from './lib/CryptoInterceptor.mjs'
 import HeaderInterceptor from './lib/HeaderInterceptor.mjs'
+import WebHttpError from './WebHttpError.mjs'
 
 const DEFAULT_CONFIG = {
   retryDelay: axiosRetry.exponentialDelay
@@ -45,11 +46,50 @@ export default class WebHttp {
       options.webHttpContext = this.context
       options.webHttpConfig = { ...this.webHttpConfig, ...webHttpConfig }
 
-      console.log('options.webHttpConfig', options.webHttpConfig)
       const response = await this.client.request(options)
       return response
     } catch (error) {
-      console.log('error', error)
+      const { request, response } = error
+      // Handle Axios Response Error
+      if (response) {
+        const { status, data: body } = response
+        const {
+          statusCode = 500,
+          message = 'Internal Server Error',
+          error: err = {}
+        } = body
+        const { errorCode, publicKey } = err
+
+        if (errorCode === 'API_CRYPTO::PRIVATE_KEY_NOT_FOUND') {
+          this.context.set(CONTEXT.PUBLIC_KEY, publicKey)
+          return await this.request(options)
+        }
+
+        const eMap = {
+          statusCode: (statusCode || status),
+          message,
+          errorCode
+        }
+        throw new WebHttpError(body, eMap)
+      }
+
+      // Handle Axios Request Error
+      if (request) {
+        const { message } = error
+        const errorParams = {
+          statusCode: -1,
+          message,
+          errorCode: 'WEN_HTTP::NETWORK'
+        }
+        throw new WebHttpError(error, errorParams)
+      }
+
+      // Handle any other form of error
+      const errorParams = {
+        statusCode: -2,
+        errorCode: 'WEN_HTTP::UNKWON'
+      }
+      throw new WebHttpError(error, errorParams)
     }
   }
 
