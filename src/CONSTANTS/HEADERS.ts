49acca83453b21a2595d4ebac4fe7a780e5a5637
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-10 00:40:25 +0530

    Change enum to const

--- src/CONSTANTS/HEADERS.ts
+++ src/CONSTANTS/HEADERS.ts
@@ -1,17 +1,16 @@
-enum REQ {
-  SESSION_ID = 'x-session-id',
-  REQUEST_ID = 'x-request-id',
-  API_KEY = 'x-api-key',
-  AUTH_TOKEN = 'x-authtoken',
-  ACCESS_TOKEN = 'x-access-token',
-  ENCRYPTION_KEY = 'x-api-encryption-key',
-  CLIENT_ID = 'x-client-id'
+export const WEB_HTTP_HEADERS = {
+  REQ: {
+    SESSION_ID: 'x-session-id',
+    REQUEST_ID: 'x-request-id',
+    API_KEY: 'x-api-key',
+    AUTH_TOKEN: 'x-authtoken',
+    ACCESS_TOKEN: 'x-access-token',
+    ENCRYPTION_KEY: 'x-api-encryption-key',
+    CLIENT_ID: 'x-client-id'
+  },
+  RES: {
+    AUTH_TOKEN: 'x-authtoken',
+    ACCESS_TOKEN: 'x-access-token',
+    REFRESH_TOKEN: 'x-refresh-token'
+  }
 }
-
-enum RES {
-  AUTH_TOKEN = 'x-authtoken',
-  ACCESS_TOKEN = 'x-access-token',
-  REFRESH_TOKEN = 'x-refresh-token'
-}
-
-export const WEB_HTTP_HEADERS = { REQ, RES }
