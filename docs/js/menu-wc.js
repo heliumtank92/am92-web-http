892bb33d658036a74b89745bf5bda77247035839
Ankit Gandhi <fc19061@superaxiss-MacBook-Pro.local>
2023-06-19 17:00:05 +0530

    Docs

--- docs/js/menu-wc.js
+++ docs/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">JoseCryptoSubtle</a>
+                    <a href="index.html" data-type="index-link">WebHttp</a>
                 </li>
 
                 <li class="divider"></li>
